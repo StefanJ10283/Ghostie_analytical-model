8b0e1774a7a0a4a1ced12e0b11e3475a4e357a08
Stefan Jonathan <s.jonathan@student.unsw.edu.au>
2026-03-14 15:26:29 +1100

    Added template file and swagger back for deployment

--- main.py
+++ main.py
@@ -113,5 +113,8 @@ def retrieve(
 
     return JSONResponse(content=result)
 
+from mangum import Mangum
+handler = Mangum(app)
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8002)
